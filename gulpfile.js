d3b60bb2882dc1c3f6ce7d69e3b4329d6e297b2b
Voyager7 <kenningspath@gmail.com>
2021-06-08 11:17:27 -0700

    Finished it out

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,8 @@
-const { series, watch } = require("gulp")
+const { series, watch, src, dest } = require("gulp")
+const sass = require("gulp-sass")
+const postcss = require("gulp-postcss")
+const cssnano = require("cssnano")
+const terser = require("gulp-terser")
 const browsersync = require("browser-sync").create()
 
 function serve(cb) {
@@ -10,13 +14,31 @@ function serve(cb) {
   cb()
 }
 
+function html() {
+  return src("*.html").pipe(dest("dist"))
+}
+
+function scss() {
+  return src("src/scss/*.scss", { sourcemaps: true })
+    .pipe(sass())
+    .pipe(postcss([cssnano()]))
+    .pipe(dest("dist", { sourcemaps: "." }))
+}
+
+function js() {
+  return src("src/js/*.js", { sourcemaps: true })
+    .pipe(terser())
+    .pipe(dest("dist", { sourcemaps: "." }))
+}
+
 function reload(cb) {
   browsersync.reload()
   cb()
 }
 
 function watchForChanges() {
-  watch("*.html", reload)
+  watch("*.html", series(html, reload))
+  watch(["src/scss/*.scss", "src/js/*.js"], series(scss, js, reload))
 }
 
-exports.default = series(serve, watchForChanges)
+exports.default = series(html, scss, js, serve, watchForChanges)
